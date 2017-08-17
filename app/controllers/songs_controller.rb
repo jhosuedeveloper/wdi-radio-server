eb25fd13b55a0f15e3f328132f8c036ad2efcca5
Josue Rosales <jhosue.rosales@gmail.com>
2017-08-16 21:41:39 -0400

    working with create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,6 +10,22 @@ class SongsController < ApplicationController
 
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(passing_info)
+    respond_to do |format|
+      if @song.save!
+        format.html { redirect_to @song, notice: 'song was successfully created.' }
+        format.json { render json: @song, status: :created, location: @song }
+      else
+        format.html { render :new }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 
 
@@ -20,7 +36,11 @@ class SongsController < ApplicationController
 
 
 
+  private
 
+  def passing_info
+    params.require(:song).permit(:title, :album, :preview_url )
+  end
 
 
 
