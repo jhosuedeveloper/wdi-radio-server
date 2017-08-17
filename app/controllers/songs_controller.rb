d71ef871219fba1be21d1b94dc8de950d304a3a1
Josue Rosales <jhosue.rosales@gmail.com>
2017-08-16 20:19:09 -0400

    list working

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @songs}
+    end
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
